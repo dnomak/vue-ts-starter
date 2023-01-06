000fa61f81fbd894eaf3f27dbbbf24ce14f9fdc9
dnomak <me@dnomak.com>
2023-01-07 02:34:15 +0300

    Update homepage design

--- src/app/App.vue
+++ src/app/App.vue
@@ -12,11 +12,49 @@ export default {
 <template lang="pug">
 div(:class="`route-${$route.name?.toString()}`")
   Options
-  font-awesome-icon(icon="fa-brands fa-vuejs")
-  font-awesome-icon(icon="fa-solid fa-heart")
-  font-awesome-icon(icon="fa-regular fa-heart")
-  ul
-    li
-      router-link(to="/") {{ $t('Home') }}
-  router-view
+  .wrap.xl-flexbox.xl-center.xl-1.container
+    .col
+      router-view
+      a.dnomak.wrap.xl-auto.xl-middle.xl-center.xl-gutter-4(href="https://github.com/dnomak" target="_blank")
+        .col I
+        .col.love.icon
+          font-awesome-icon(icon="fa-regular fa-heart")
+          font-awesome-icon(icon="fa-solid fa-heart")
+        .col.love Love
+        .col JavaScript
 </template>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+  padding: 24rem;
+}
+.dnomak {
+  font-family: var(--source-code-pro);
+  display: inline-block;
+  color: var(--gray-500);
+  font-weight: 600;
+  line-height: 24rem;
+  .col {
+    font-size: 16rem;
+  }
+  .love {
+    color: var(--red-300);
+    &.icon {
+      line-height: 0;
+      font-size: 24rem;
+    }
+  }
+  [data-prefix='fas'] {
+    display: none;
+  }
+  &:hover {
+    [data-prefix='far'] {
+      display: none;
+    }
+    [data-prefix='fas'] {
+      display: inline-block;
+    }
+  }
+}
+</style>
